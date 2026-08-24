1f3da059df3f7ea905a047f00d1f3784bd5d10f0
jayesh11787 <jayeshp58@gmail.com>
2026-08-24 18:05:20 +0530

    debug

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function beforeSubmit(){
     let outputdate = document.querySelector('.outputdate');
     let inputdate = document.querySelector('.inputdate');
-
+    console.log(inputdate.value);
     let formatedDate = new Date(inputdate.value).toLocaleDateString("en-US");
     outputdate.value = formatedDate;
 }
